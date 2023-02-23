1468370e6aaeb4d4da45af8eca4496863712ef82
renatodch <renato.delcas@gmail.com>
2023-02-23 17:08:39 -0500

    3 components added

--- my-lib/src/components.d.ts
+++ my-lib/src/components.d.ts
@@ -11,11 +11,22 @@ export namespace Components {
         "opened": boolean;
         "title": string;
     }
+    interface UcSpinner {
+    }
+    interface UcStockFinder {
+    }
+    interface UcStockPrice {
+        "stockSymbol": string;
+    }
     interface UcTooltip {
         "content": string;
         "show": () => Promise<void>;
         "showed": boolean;
     }
+}
+export interface UcStockFinderCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLUcStockFinderElement;
 }
 declare global {
     interface HTMLUcSideDrawerElement extends Components.UcSideDrawer, HTMLStencilElement {
@@ -23,6 +34,24 @@ declare global {
     var HTMLUcSideDrawerElement: {
         prototype: HTMLUcSideDrawerElement;
         new (): HTMLUcSideDrawerElement;
+    };
+    interface HTMLUcSpinnerElement extends Components.UcSpinner, HTMLStencilElement {
+    }
+    var HTMLUcSpinnerElement: {
+        prototype: HTMLUcSpinnerElement;
+        new (): HTMLUcSpinnerElement;
+    };
+    interface HTMLUcStockFinderElement extends Components.UcStockFinder, HTMLStencilElement {
+    }
+    var HTMLUcStockFinderElement: {
+        prototype: HTMLUcStockFinderElement;
+        new (): HTMLUcStockFinderElement;
+    };
+    interface HTMLUcStockPriceElement extends Components.UcStockPrice, HTMLStencilElement {
+    }
+    var HTMLUcStockPriceElement: {
+        prototype: HTMLUcStockPriceElement;
+        new (): HTMLUcStockPriceElement;
     };
     interface HTMLUcTooltipElement extends Components.UcTooltip, HTMLStencilElement {
     }
@@ -32,6 +61,9 @@ declare global {
     };
     interface HTMLElementTagNameMap {
         "uc-side-drawer": HTMLUcSideDrawerElement;
+        "uc-spinner": HTMLUcSpinnerElement;
+        "uc-stock-finder": HTMLUcStockFinderElement;
+        "uc-stock-price": HTMLUcStockPriceElement;
         "uc-tooltip": HTMLUcTooltipElement;
     }
 }
@@ -40,12 +72,23 @@ declare namespace LocalJSX {
         "opened"?: boolean;
         "title"?: string;
     }
+    interface UcSpinner {
+    }
+    interface UcStockFinder {
+        "onUcSymbolSelected"?: (event: UcStockFinderCustomEvent<string>) => void;
+    }
+    interface UcStockPrice {
+        "stockSymbol"?: string;
+    }
     interface UcTooltip {
         "content"?: string;
         "showed"?: boolean;
     }
     interface IntrinsicElements {
         "uc-side-drawer": UcSideDrawer;
+        "uc-spinner": UcSpinner;
+        "uc-stock-finder": UcStockFinder;
+        "uc-stock-price": UcStockPrice;
         "uc-tooltip": UcTooltip;
     }
 }
@@ -54,6 +97,9 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "uc-side-drawer": LocalJSX.UcSideDrawer & JSXBase.HTMLAttributes<HTMLUcSideDrawerElement>;
+            "uc-spinner": LocalJSX.UcSpinner & JSXBase.HTMLAttributes<HTMLUcSpinnerElement>;
+            "uc-stock-finder": LocalJSX.UcStockFinder & JSXBase.HTMLAttributes<HTMLUcStockFinderElement>;
+            "uc-stock-price": LocalJSX.UcStockPrice & JSXBase.HTMLAttributes<HTMLUcStockPriceElement>;
             "uc-tooltip": LocalJSX.UcTooltip & JSXBase.HTMLAttributes<HTMLUcTooltipElement>;
         }
     }
